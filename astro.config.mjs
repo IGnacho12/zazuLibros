e58dc0e7de7dd79b742415290ec2ef9b96bc0daa
nacho <castilloignacio1510@gmail.com>
2025-09-14 07:43:25 -0300

    logramos obtener libros de una base de datos y renderizarlos con pdf.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server", // 🔑 así todas las rutas dinámicas se sirven en runtime
   vite: {
     plugins: [tailwindcss()]
   }
---
+++ src/pages/api/getBooks.ts
@@ -0,0 +1,19 @@
+import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
+export const prerender = false;
+
+
+dotenv.config();
+const consulta = neon(process.env.DATABASE_URL);
+
+export async function GET() {
+  const resultado = await consulta`
+    SELECT id, nombre, autor, archivo_url, calificaciones, created_at
+    FROM pdfs
+    ORDER BY created_at DESC
+  `;
+  return new Response(JSON.stringify(resultado), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
